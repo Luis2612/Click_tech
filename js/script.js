e3e0b9edb6509c1e101bc5aa60fa06613a7e3548
Luis2612 <luisimba2612@gmail.com>
2026-07-15 14:38:21 -0500

    Integrate login and register authentication with LocalStorage and unified page routing

--- js/script.js
+++ js/script.js
@@ -46,6 +46,7 @@ document.addEventListener("DOMContentLoaded", () => {
       observer.observe(el);
     }
   });
+  validarRutasAutorizadas();
 });
 
 if(document.getElementById('contactForm') != undefined){
@@ -79,6 +80,130 @@ if(document.getElementById('authenticationtForm') != undefined){
         document.getElementById('formStatus').classList.remove('d-none');
       }
     });
+}
+
+if (document.getElementById("inicioSesion") != undefined) {
+  document
+    .getElementById("inicioSesion")
+    .addEventListener("click", function (event) {
+      event.preventDefault();
+      sessionStorage.removeItem("usuarioAutenticado");
+      window.location.href = "../login/index.html";
+    });
+}
+
+if (document.getElementById("registerForm") != undefined) {
+  document
+    .getElementById("registerForm")
+    .addEventListener("submit", function (event) {
+      event.preventDefault();
+      if (this.checkValidity()) {
+        registrarUsuario();
+      } else {
+        event.stopPropagation();
+        this.classList.add("was-validated");
+      }
+    });
+}
+
+if (document.getElementById("loginForm") != undefined) {
+  document
+    .getElementById("loginForm")
+    .addEventListener("submit", function (event) {
+      event.preventDefault();
+      if (this.checkValidity()) {
+        let email = document.getElementById("email").value;
+        let password = document.getElementById("password").value;
+
+        if (autenticarse(email, password)) {
+          window.location.href = "../contact/index.html";
+        } else {
+          mostrarMensajeError("¡Error! usuario y contraseña invalido.");
+        }
+      } else {
+        event.stopPropagation();
+        this.classList.add("was-validated");
+      }
+    });
+}
+
+function mostrarMensajeError(mensaje) {
+  var contenedor = document.getElementById("contenedorErrores");
+  contenedor.innerHTML = `<div class="alert alert-danger alert-dismissible fade show" role="alert">
+            ${mensaje}
+            <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
+            </div>
+            `;
+}
+
+function autenticarse(email, password) {
+  let autenticado = false;
+  let usuarios = JSON.parse(localStorage.getItem("usuarios")) || [];
+  if (usuarios.length > 0) {
+    let usuario = usuarios.find(
+      (user) => user.email === email && user.password === password,
+    );
+
+    if (usuario) {
+      sessionStorage.setItem("usuarioAutenticado", JSON.stringify(usuario));
+      autenticado = true;
+    }
+  }
+  return autenticado;
+}
+
+function registrarUsuario() {
+  let usuarios = JSON.parse(localStorage.getItem("usuarios")) || [];
+
+  let nombre = document.getElementById("nombre").value;
+  let correo = document.getElementById("email").value;
+  let telefono = document.getElementById("telefono").value;
+  let password = document.getElementById("password").value;
+
+  let usuario = usuarios.find((user) => user.email === correo);
+
+  if (usuario) {
+    mostrarMensajeError("¡Error! El correo ya existe en el sistema.");
+  } else {
+    let usuario = {
+      nombre: nombre,
+      email: correo,
+      telefono: telefono,
+      password: password,
+    };
+
+    usuarios.push(usuario);
+    localStorage.setItem("usuarios", JSON.stringify(usuarios));
+    window.location.href = "../login/index.html";
+  }
+}
+
+function validarRutasAutorizadas(){
+  let usuario = JSON.parse(sessionStorage.getItem("usuarioAutenticado")) || null;
+
+  if (usuario) {
+    let linkInicioSesion = document.getElementById("inicioSesion");
+    if (linkInicioSesion) {
+      linkInicioSesion.innerHTML = "Cerrar sesión";
+    }
+  }
+
+  if (window.location.href.includes("/login/index.html")) {
+    if (usuario != null) {
+      window.location.href = "../home/index.html";
+    }
+  } else {
+    if (usuario == null) {
+      if (
+        !window.location.href.includes("/login/index.html") &&
+        !window.location.href.includes("/register/index.html") &&
+        !window.location.href.includes("/catalogo/index.html") &&
+        !window.location.href.includes("/catalogo/product.html")
+      ) {
+        window.location.href = "../login/index.html";
+      }
+    }
+  }
 }
 
     
